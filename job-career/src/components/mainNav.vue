0434ffe29af7d2d24837cf10dc10c5f96f26d407
Vedant Gonnade <vedantgonnade@outlook.com>
2024-12-11 20:11:41 +0100

    used data in vue component and displayed it on DOM

--- job-career/src/components/mainNav.vue
+++ job-career/src/components/mainNav.vue
@@ -1,10 +1,13 @@
 <template>
-  <header class="w-full text-start">
+  <header class="w-full text-sm">
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div
         class="border-brand-gray-1 mx-auto flex h-full flex-nowrap border-b border-solid px-8"
       >
-        <a href="/" class="flex h-full items-center text-xl">Vedant Career</a>
+        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+        <h2 class="ml-5 flex h-full items-center">
+          Developed by {{ author.firstName }} {{ author.lastName }}
+        </h2>
       </div>
     </div>
   </header>
@@ -13,5 +16,14 @@
 <script>
 export default {
   name: "mainNav",
+  data() {
+    return {
+      company: "Awesome website",
+      author: {
+        firstName: "Vedant",
+        lastName: "Gonnade",
+      },
+    };
+  },
 };
 </script>
