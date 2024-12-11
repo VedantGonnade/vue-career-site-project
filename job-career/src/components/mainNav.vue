be799b902ed5d48840c27f05c5401b47b2da7e36
Vedant Gonnade <vedantgonnade@outlook.com>
2024-12-11 15:56:14 +0100

    created the nav bar using tailwind classes

--- job-career/src/components/mainNav.vue
+++ job-career/src/components/mainNav.vue
@@ -1,5 +1,13 @@
 <template>
-  <header class="w-full text-start">I am Main nav</header>
+  <header class="w-full text-start">
+    <div class="fixed left-0 top-0 h-16 w-full bg-white">
+      <div
+        class="border-brand-gray-1 mx-auto flex h-full flex-nowrap border-b border-solid px-8"
+      >
+        <a href="/" class="flex h-full items-center text-xl">Vedant Career</a>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
