056570514b85d9c760d03a4d36f40d33e34fd868
Vedant Gonnade <vedantgonnade@outlook.com>
2024-12-21 20:01:23 +0100

    used v-bind_key with v-for

--- job-career/src/components/mainNav.vue
+++ job-career/src/components/mainNav.vue
@@ -1,11 +1,19 @@
 <template>
   <header class="w-full text-sm">
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
-      <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
-        <a v-bind:href="url" class="flex h-full items-center text-xl"> {{ company }}</a>
+      <div
+        class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
+      >
+        <a v-bind:href="url" class="flex h-full items-center text-xl">
+          {{ company }}</a
+        >
         <nav class="ml-12 h-full">
           <ul class="flex h-full items-center">
-            <li class="ml-7 py-2 font-medium" v-for="menuItem in menuItems">
+            <li
+              v-for="menuItem in menuItems"
+              v-bind:key="menuItem"
+              class="ml-7 py-2 font-medium"
+            >
               <a href="">{{ menuItem }}</a>
             </li>
           </ul>
@@ -22,7 +30,14 @@ export default {
     return {
       company: "Awesome website",
       url: "https://careers.google.com",
-      menuItems: ["Teams", "Locations", "Life at Bobo Corp", "How we hire", "Students", "Jobs"],
+      menuItems: [
+        "Teams",
+        "Locations",
+        "Life at Bobo Corp",
+        "How we hire",
+        "Students",
+        "Jobs",
+      ],
     };
   },
 };
