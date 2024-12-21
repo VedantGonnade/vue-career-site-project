0e7f6466035bcd778fb502d46c071038a143068e
Vedant Gonnade <vedantgonnade@outlook.com>
2024-12-21 19:18:00 +0100

    used v-for directive

--- job-career/src/components/mainNav.vue
+++ job-career/src/components/mainNav.vue
@@ -1,10 +1,15 @@
 <template>
   <header class="w-full text-sm">
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
-      <div
-        class="border-brand-gray-1 mx-auto flex h-full flex-nowrap border-b border-solid px-8"
-      >
-        <a href="/" class="flex h-full items-center text-xl"> {{ company }}</a>
+      <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
+        <a v-bind:href="url" class="flex h-full items-center text-xl"> {{ company }}</a>
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full items-center">
+            <li class="ml-7 py-2 font-medium" v-for="menuItem in menuItems">
+              <a href="">{{ menuItem }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -16,6 +21,8 @@ export default {
   data() {
     return {
       company: "Awesome website",
+      url: "https://careers.google.com",
+      menuItems: ["Teams", "Locations", "Life at Bobo Corp", "How we hire", "Students", "Jobs"],
     };
   },
 };
