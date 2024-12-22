f5e1718ed7a4559b1bbf1bb5f1ce3270344f4e0f
Vedant Gonnade <vedantgonnade@outlook.com>
2024-12-22 16:10:50 +0100

    added second test by using getAllByRole method

--- job-career/tests/unit/components/mainNav.test.js
+++ job-career/tests/unit/components/mainNav.test.js
@@ -1,12 +1,28 @@
 import { render, screen } from "@testing-library/vue";
 
 import mainNav from "@/components/mainNav.vue";
-import { describe, expect } from "vitest";
+import { expect } from "vitest";
 
 describe("mainNav", () => {
   it("it displays company name", () => {
     render(mainNav);
     const companyName = screen.getByText("Awesome website");
     expect(companyName).toBeInTheDocument();
+  });
+
+  it("displays menu items for navigation", () => {
+    render(mainNav);
+    const navigationMenuItems = screen.getAllByRole("listitem");
+    const navigationMenuTexts = navigationMenuItems.map(
+      item => item.textContent,
+    );
+    expect(navigationMenuTexts).toStrictEqual([
+      "Teams",
+      "Locations",
+      "Life at Bobo Corp",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
   });
 });
