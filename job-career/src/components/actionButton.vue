cac1f8d1ee1d51d0d9e4d4acd47d8d58ee2827db
Vedant Gonnade <vedantgonnade@outlook.com>
2024-12-25 16:24:58 +0100

    used props instead of hardcoding name of the button

--- job-career/src/components/actionButton.vue
+++ job-career/src/components/actionButton.vue
@@ -2,12 +2,13 @@
   <button
     class="rounded-lg bg-brand-blue-1 px-4 py-2 text-white hover:shadow-blue"
   >
-    Sign up
+    {{ text }}
   </button>
 </template>
 
 <script>
 export default {
   name: "actionButton",
+  props: ["text"],
 };
 </script>
--- job-career/src/components/mainNav.vue
+++ job-career/src/components/mainNav.vue
@@ -22,7 +22,7 @@
         </nav>
         <div class="ml-auto flex h-full items-center">
           <ProfileImage v-if="isLoggedIn" />
-          <ActionButton v-else @click="loginUser" />
+          <ActionButton v-else text="Sign in" @click="loginUser" />
         </div>
       </div>
     </div>
