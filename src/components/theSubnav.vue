edfc1e9e8a7dbe0ce43f9a07830dd6e5e527162a
Vedant Gonnade <vedantgonnade@outlook.com>
2024-12-26 18:20:50 +0100

    modifying subnav component

--- src/components/theSubnav.vue
+++ src/components/theSubnav.vue
@@ -1,10 +1,7 @@
 <template>
-  <div
-    v-if="onJobResultsPage"
-    class="h-16 w-full border-b border-solid border-brand-gray-1 bg-white"
-  >
+  <div class="h-16 w-full border-b border-solid border-brand-gray-1 bg-white">
     <div class="flex h-full items-center px-8">
-      <div>
+      <div v-if="onJobResultsPage">
         <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
         <span><span class="text-brand-green-1">1653</span> jobs matched</span>
       </div>
@@ -17,7 +14,7 @@ export default {
   name: "TheSubnav",
   data() {
     return {
-      onJobResultsPage: true,
+      onJobResultsPage: false,
     };
   },
 };
