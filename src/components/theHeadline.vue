8d1df341ed717b1a96b47f453a9291a5c9f7e490
Vedant Gonnade <vedantgonnade@outlook.com>
2024-12-28 23:11:33 +0100

    added dynamic style class selection and basic style for headline

--- src/components/theHeadline.vue
+++ src/components/theHeadline.vue
@@ -1,11 +1,15 @@
 <template>
   <section>
-    <h1>{{ action }} for everyone</h1>
-    <h2>Find your next job at Zonal Surf</h2>
+    <h1 class="mb-14 text-8xl font-bold tracking-tighter">
+      <span :class="changeActionStyle">{{ action }}</span> <br />
+      for everyone
+    </h1>
+    <h2 class="text-3xl font-light">Find your next job at Zonal Surf</h2>
   </section>
 </template>
 
 <script>
+
 export default {
   name: "TheHeadline",
   data() {
@@ -13,6 +17,16 @@ export default {
       action: "Build",
       interval: null,
     };
+  },
+  computed: {
+    changeActionStyle() {
+      return {
+        build: this.action === "Build",
+        create: this.action === "Create",
+        design: this.action === "Design",
+        code: this.action === "Code",
+      };
+    },
   },
   created() {
     this.changeAction();
@@ -32,3 +46,18 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.build {
+  color: #1a73e8;
+}
+.create {
+  color: #34a853;
+}
+.design {
+  color: #f9ab00;
+}
+.code {
+  color: #d93025;
+}
+</style>
