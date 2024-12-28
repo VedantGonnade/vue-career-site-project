c65264ef396e0e985049c21104467d8d738ec561
Vedant Gonnade <vedantgonnade@outlook.com>
2024-12-28 22:47:20 +0100

    added headline component

---
+++ src/components/theHeadline.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <h1>{{ action }} for everyone</h1>
+    <h2>Find your next job at Zonal Surf</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "TheHeadline",
+  data() {
+    return {
+      action: "Build",
+      interval: null,
+    };
+  },
+  created() {
+    this.changeAction();
+  },
+  beforeUnmount() {
+    clearInterval(this.interval);
+  },
+  methods: {
+    changeAction() {
+      this.interval = setInterval(() => {
+        const actions = ["Build", "Create", "Design", "Code"];
+        const actionIndex = actions.indexOf(this.action);
+        const nextActionIndex = (actionIndex + 1) % actions.length;
+        this.action = actions[nextActionIndex];
+      }, 3000);
+    },
+  },
+};
+</script>
