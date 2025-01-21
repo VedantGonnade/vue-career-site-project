3cee6836c8177f75ccea9328b90b2a6c487e0ff0
Vedant Gonnade <vedantgonnade@outlook.com>
2025-01-21 21:01:53 +0100

    used axios to get data and display 100 jobs on job page

--- src/components/jobResults/jobListings.vue
+++ src/components/jobResults/jobListings.vue
@@ -1,20 +1,28 @@
 <template>
   <main class="flex-auto bg-brand-gray-2 p-8">
     <ol>
-      <JobListing />
-      <JobListing />
-      <JobListing />
+      <JobListing v-for="job in jobs" :key="job.id" :job="job" />
     </ol>
   </main>
 </template>
 
 <script>
 import JobListing from "@/components/jobResults/jobListing.vue";
+import axios from "axios";
 
 export default {
   name: "JobListings",
   components: {
     JobListing,
+  },
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  async mounted() {
+    const { data } = await axios.get("http://localhost:3000/jobs");
+    this.jobs = data;
   },
 };
 </script>
