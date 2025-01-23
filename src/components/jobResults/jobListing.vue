a6ad43e448c70723b4c760c14facd7c732f7ee53
Vedant Gonnade <vedantgonnade@outlook.com>
2025-01-23 14:57:07 +0100

    used props to display actual jobs

--- src/components/jobResults/jobListing.vue
+++ src/components/jobResults/jobListing.vue
@@ -1,31 +1,39 @@
 <template>
   <li class="mb-7">
     <RouterLink
-      to="/jobs/results/1"
-      class="hover:shadow-gray mx-auto block rounded border border-solid border-brand-gray-2 bg-white"
+      :to="jobPageLink"
+      class="mx-auto block rounded border border-solid border-brand-gray-2 bg-white hover:shadow-gray"
     >
       <div class="mx-8 pb-2 pt-5">
         <h2 class="mb-2 text-2xl">
-          Technical Program Manager, Perception, Augmentd Reality
+          {{ job.title }}
         </h2>
         <div class="flex flex-row align-middle">
           <div class="mr-5">
-            <span>Bobo</span>
+            <span>{{ job.organization }}</span>
           </div>
           <div>
-            <span>San Francisco, CA, USA</span>
+            <ul>
+              <li
+                v-for="location in job.locations"
+                :key="location"
+                class="mr-3 inline-block"
+              >
+                <span>{{ location }}</span>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
       <div class="px-8 py-4">
-        <h3 class="mb-2 mt-1">Qualifications</h3>
+        <h3 class="mb-2 mt-1">Qualifications:</h3>
         <div>
           <ul class="list-disc pl-8">
-            <li>Bachelors degree or equivalent practical experience.</li>
-            <li>5 years of experience in program management</li>
-            <li>
-              Experience analyzing data through querying databse (e.g., SQL)
-              using spreadsheet software, and creating statistical models.
+            <li
+              v-for="qualification in job.minimumQualifications"
+              :key="qualification"
+            >
+              {{ qualification }}
             </li>
           </ul>
         </div>
@@ -37,5 +45,16 @@
 <script>
 export default {
   name: "JobListing",
+  props: {
+    job: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    jobPageLink() {
+      return `/jobs/results/${this.job.id}`;
+    },
+  },
 };
 </script>
