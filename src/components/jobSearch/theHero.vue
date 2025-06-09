5db1c572b180d711c40c05d771c56606b9381421
Vedant Gonnade <vedantgonnade@outlook.com>
2025-06-09 11:52:11 +0200

    added spotlight component to the main page by making use of scoped slots

--- src/components/jobSearch/theHero.vue
+++ src/components/jobSearch/theHero.vue
@@ -1,6 +1,6 @@
 <template>
-  <main>
-    <section class="flex h-screen flex-col pb-20 pt-10">
+  <main class="flex h-screen flex-col">
+    <section class="pb-20 pt-10">
       <div class="grid grid-cols-12">
         <div class="col-span-1 col-start-1"></div>
         <div class="col-span-5 col-start-2">
@@ -17,6 +17,27 @@
         <div class="col-span-1 col-start-auto"></div>
       </div>
     </section>
+
+    <Spotlight class="flex flex-row justify-center pb-16">
+      <template #default="{ img, title, description }">
+        <RouterLink
+          to="/jobs/results"
+          class="mx-5 flex h-96 w-72 flex-col rounded-lg border bg-brand-gray-2"
+        >
+          <img :src="img" class="object-contain" />
+          <div class="mt-3 h-48 px-6 py-4">
+            <h3 class="text-lg font-medium">{{ title }}</h3>
+            <p class="mt-3 text-sm">{{ description }}</p>
+          </div>
+
+          <RouterLink
+            to="/jobs/results"
+            class="px-6 pb-4 text-sm text-brand-blue-1"
+            >See Jobs</RouterLink
+          >
+        </RouterLink>
+      </template>
+    </Spotlight>
   </main>
 </template>
 
@@ -24,12 +45,14 @@
 import TheHeadline from "@/components/jobSearch/theHeadline.vue";
 import JobSearchForm from "@/components/jobSearch/jobSearchForm.vue";
 import VueImage from "@/images/Vue.js_Logo_2.svg.png";
+import Spotlight from "@/components/jobSearch/spotLight.vue";
 
 export default {
   name: "TheHero",
   components: {
     TheHeadline,
     JobSearchForm,
+    Spotlight,
   },
   data() {
     return {
