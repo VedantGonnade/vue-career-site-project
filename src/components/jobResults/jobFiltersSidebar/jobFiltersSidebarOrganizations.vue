60272f75293f48122d80c5145a5c6ae9be8661eb
Vedant Gonnade <vedantgonnade@outlook.com>
2025-06-10 18:45:46 +0200

    using pinia getters to find unique organization

--- src/components/jobResults/jobFiltersSidebar/jobFiltersSidebarOrganizations.vue
+++ src/components/jobResults/jobFiltersSidebar/jobFiltersSidebarOrganizations.vue
@@ -3,24 +3,15 @@
     <div class="mt-5">
       <fieldset>
         <ul class="flex flex-row flex-wrap">
-          <li class="h-8 w-1/2">
-            <input id="VueTube" type="checkbox" class="mr-3" />
-            <label for="VueTube" class="text-sm">VueTube</label>
-          </li>
-
-          <li class="h-8 w-1/2">
-            <input id="VueTube" type="checkbox" class="mr-3" />
-            <label for="VueTube" class="text-sm">VueTube</label>
-          </li>
-
-          <li class="h-8 w-1/2">
-            <input id="VueTube" type="checkbox" class="mr-3" />
-            <label for="VueTube" class="text-sm">VueTube</label>
-          </li>
-
-          <li class="h-8 w-1/2">
-            <input id="VueTube" type="checkbox" class="mr-3" />
-            <label for="VueTube" class="text-sm">VueTube</label>
+          <li
+            v-for="organization in UNIQUE_ORGANIZATION"
+            :key="organization"
+            class="flex h-8 w-1/2 items-center justify-start p-2"
+          >
+            <input :id="organization" type="checkbox" class="mr-3" />
+            <label :for="organization" class="text-sm">{{
+              organization
+            }}</label>
           </li>
         </ul>
       </fieldset>
@@ -30,10 +21,15 @@
 
 <script>
 import CollapsibleAccordian from "@/components/shared/collapsibleAccordian.vue";
+import { useJobsStore, UNIQUE_ORGANIZATION } from "@/stores/jobs";
+import { mapState } from "pinia";
 export default {
   name: "JobFiltersSidebarOrganizations",
   components: {
     CollapsibleAccordian,
+  },
+  computed: {
+    ...mapState(useJobsStore, [UNIQUE_ORGANIZATION]),
   },
 };
 </script>
--- src/stores/jobs.js
+++ src/stores/jobs.js
@@ -2,6 +2,7 @@ import getJobs from "@/api/getJobs";
 import { defineStore } from "pinia";
 
 export const FETCH_JOBS = "FETCH_JOBS";
+export const UNIQUE_ORGANIZATION = "UNIQUE_ORGANIZATION";
 
 export const useJobsStore = defineStore("jobs", {
   state: () => ({
@@ -10,6 +11,17 @@ export const useJobsStore = defineStore("jobs", {
   actions: {
     async [FETCH_JOBS]() {
       this.jobs = await getJobs();
+    },
+  },
+  getters: {
+    [UNIQUE_ORGANIZATION](state) {
+      const uniqueOrganizations = new Set();
+      state.jobs.forEach(job => {
+        if (job.organization) {
+          uniqueOrganizations.add(job.organization);
+        }
+      });
+      return uniqueOrganizations;
     },
   },
 });
