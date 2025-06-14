be4aa2a818265a443a59e6ac8b7e89fb23ba76d5
Vedant Gonnade <vedantgonnade@outlook.com>
2025-06-14 23:57:39 +0200

    added getters in pinia store

--- src/stores/jobs.js
+++ src/stores/jobs.js
@@ -5,6 +5,8 @@ import { useUserStore } from "@/stores/user";
 export const FETCH_JOBS = "FETCH_JOBS";
 export const UNIQUE_ORGANIZATION = "UNIQUE_ORGANIZATION";
 export const FILTERED_JOBS_BY_ORGANIZATION = "FILTERED_JOBS_BY_ORGANIZATION";
+export const UNIQUE_JOB_TYPES = "UNIQUE_JOB_TYPES";
+export const FILTERED_JOB_TYPES = "FILTERED_JOB_TYPES";
 
 export const useJobsStore = defineStore("jobs", {
   state: () => ({
@@ -35,6 +37,28 @@ export const useJobsStore = defineStore("jobs", {
 
       return state.jobs.filter(job => {
         return userStore.selectedOrganization.includes(job.organization);
+      });
+    },
+
+    [UNIQUE_JOB_TYPES](state) {
+      const uniqueJobTypes = new Set();
+      state.jobs.forEach(job => {
+        if (job.jobType) {
+          uniqueJobTypes.add(job.jobType);
+        }
+      });
+      return uniqueJobTypes;
+    },
+
+    [FILTERED_JOB_TYPES](state) {
+      const userStore = useUserStore();
+
+      if (userStore.selectedJobTypes.length === 0) {
+        return state.jobs;
+      }
+
+      return state.jobs.filter(job => {
+        return userStore.selectedJobTypes.includes(job.jobType);
       });
     },
   },
--- src/stores/user.js
+++ src/stores/user.js
@@ -1,11 +1,13 @@
 import { defineStore } from "pinia";
 
 export const ADD_SELECTED_ORGANIZATION = "ADD_SELECTED_ORGANIZATION";
+export const ADD_SELECTED_JOB_TYPES = "ADD_SELECTED_JOB_TYPES";
 
 export const useUserStore = defineStore("user", {
   state: () => ({
     isLoggedIn: false,
     selectedOrganization: [],
+    selectedJobTypes: [],
   }),
   actions: {
     login() {
@@ -14,6 +16,10 @@ export const useUserStore = defineStore("user", {
 
     [ADD_SELECTED_ORGANIZATION](organizations) {
       this.selectedOrganization = organizations;
+    },
+
+    [ADD_SELECTED_JOB_TYPES](jobTypes) {
+      this.selectedJobTypes = jobTypes;
     },
   },
 });
